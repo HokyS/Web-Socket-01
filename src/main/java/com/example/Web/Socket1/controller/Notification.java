df85d4e881df78f870685dffeff85fae23e0db5f
Mario Cotumaccio <mariocotumaccio1@gmail.com>
2022-11-07 15:06:23 +0100

    Creating Entities DTO

---
+++ src/main/java/com/example/Web/Socket1/controller/Notification.java
@@ -0,0 +1,4 @@
+package com.example.Web.Socket1.controller;
+
+public class Notification {
+}
---
+++ src/main/java/com/example/Web/Socket1/entities/DTO.java
@@ -0,0 +1,28 @@
+package com.example.Web.Socket1.entities;
+
+public class DTO {
+
+    private String type;
+    private String message;
+
+    public DTO(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
